f43470ddb99fe1b79fb137ac0339e9081545a539
Kath Oreilly <k.oreilly@imperial.ac.uk>
2016-08-03 14:28:27 +0100

    first commit

---
+++ R/mainFunctions.R
@@ -0,0 +1,11 @@
+
+#' @title MELD
+#' @name MELD
+#' @docType package
+#' @useDynLib MELD
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+
+dummy1 <- function() {
+	print('dummy1')
+}
